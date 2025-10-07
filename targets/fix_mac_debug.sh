d13c7a385df7575cd08c62dd4831ac62dc2d3700
Ivan Šachov <ivan.shakhov@jetbrains.com>
2025-10-07 09:52:26 +0200

    remove files related to macos entitlements and indexLayout.xml

--- targets/fix_mac_debug.sh
+++
@@ -1,4 +0,0 @@
-# to check current entitlements
-#codesign -d --entitlements :- /Applications/Godot_v4.4.1-stable_macos.app/Contents/MacOS/Godot
-# to overwrite
-codesign --entitlements entitlements.plist -f -s - /Applications/Godot_v4.4.1-stable_macos.app/Contents/MacOS/Godot
