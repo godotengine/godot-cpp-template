06943a562cff492179313c9546f43b66f2b8429a
Luca Ian Türk <lucatuerk@googlemail.com>
2026-03-02 10:53:36 +0100

    Added GODOT_FUNCTION attribute, some clean up ci.yml

--- src/example_class.h
+++ src/example_class.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include "godot_object_compiler/macros.h"
 #include "godot_cpp/classes/ref_counted.hpp"
 #include "godot_cpp/classes/wrapped.hpp"
 #include "godot_cpp/variant/variant.hpp"
+#include "godot_object_compiler/macros.h"
 
 using namespace godot;
 #include "example_class.generated.h"
@@ -16,6 +16,7 @@ public:
 	ExampleClass() = default;
 	~ExampleClass() override = default;
 
+	GODOT_FUNCTION();
 	void print_type(const Variant &p_variant) const;
 };
 
